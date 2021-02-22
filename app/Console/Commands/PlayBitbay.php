e6d9be7f8b1b854fc27c4fb5357c1aba4650f501
filuus <filuuuus@gmail.com>
2021-02-22 08:38:37 +0100

    change quantity of repeat

--- app/Console/Commands/PlayBitbay.php
+++ app/Console/Commands/PlayBitbay.php
@@ -63,7 +63,7 @@ class PlayBitbay extends Command
 
         $mlp = new MLPClassifier(60, [30], [-1, 0, 1]);
 
-        for ($i = 0; $i < 400; $i++) {
+        for ($i = 0; $i < 300; $i++) {
             $quantity = (string)($i * 5);
             $result = $this->getSamples(Carbon::now('Europe/Warsaw')->sub($quantity . 'minutes'));
             $mlp->partialTrain(
