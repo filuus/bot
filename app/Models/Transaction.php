79b300e9c5c6f312453dd278506252088813d97f
Filip <filuuuus@gmail.com>
2021-03-20 20:03:34 +0100

    fix service

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -70,7 +70,7 @@ class Transaction extends Model
      */
     public function getProfitAttribute() {
         $lastId = Transaction::latest()->first()->id;
-        if ($this->type === self::SELL || $this->id === 1) {
+        if ($this->type === self::BUY || $this->id === 1) {
             return 0;
         }
         $prevBalance = Transaction::find($this->id - 1)->balance;
