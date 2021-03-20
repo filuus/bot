8f830f4a22e5be923311637031733e9c3060c1ff
Filip <filuuuus@gmail.com>
2021-03-20 20:13:25 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,13 +44,14 @@
                     <td class="{{ $transaction->type === $sell ? 'red' : 'green' }}">{{ $transaction->type }}</td>
                     <td>{{ $transaction->formatted_amount }}</td>
                     <td>{{ $transaction->formatted_rate }}</td>
-                    @if ($transaction->profit > 0)
+                    <td>{{ $transaction->profit }}</td>
+                    <!-- @if ($transaction->profit > 0)
                         <td class="green">{{ number_format($transaction->profit, 2, ',', ' ') }} PLN</td>
                     @elseif ($transaction->profit === 0)
                         <td>{{ number_format($transaction->profit, 2, ',', ' ') }} PLN</td>
                     @else
                         <td class="red">{{ number_format($transaction->profit, 2, ',', ' ') }} PLN</td>
-                    @endif
+                    @endif -->
                     <td>{{ $transaction->formatted_balance }}</td>
                     <td>{{ $transaction->updated_at }}</td>
                     <td>{{ $transaction->created_at }}</td>
