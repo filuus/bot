277df24942f2b447ffd547daf00639515b33ae8b
Filip <filuuuus@gmail.com>
2021-03-22 22:07:42 +0100

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton('network', function() {
+        $this->app->instance('network', function() {
             return new Network();
         });
     }
